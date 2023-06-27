049396b5e5ea3dd2be213b6b16c9923056adf847
Aruhan Mathias <aruhan.mathias@itlean.com.br>
2023-06-27 15:10:41 -0300

    feat: add negociacoes model

---
+++ app/models/negociacoes.ts
@@ -0,0 +1,19 @@
+import { Negociacao } from './negociacao.js';
+
+export class Negociacoes {
+
+    private _negociacoes: Array<Negociacao> = []
+
+    adiciona(negociacao: Negociacao): void {
+
+        this._negociacoes.push(negociacao)
+
+    }
+
+    lista(): ReadonlyArray<Negociacao> {
+
+        return this._negociacoes
+
+    }
+
+}
